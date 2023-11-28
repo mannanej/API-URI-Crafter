30939b9e74d40b7536ca9e099b6651816d78858f
Eddie Mannan <eddiemannan@gmail.com>
2023-11-28 11:07:24 -0500

    Wrote basic code

---
+++ main.py
@@ -0,0 +1,47 @@
+
+BASEURI = "https://localhost/MESMW/"
+REQUESTURI = "api/v3/DirectAccess/?spName="
+POSTDATA = ""
+XMLVAR = "XML_VAR_HERE"
+
+def main():
+    # Make the special variables editable
+    global REQUESTURI
+    global POSTDATA
+    # Get the name of the stored procedure
+    storedProcedure = input("Stored Procedure Name: ")
+    REQUESTURI += storedProcedure
+    # Get a list of variable names, split them into array
+    variables = input("Provide A Comma Seperated List Of Variable Names: ")
+    variables = variables.replace(" ", "")
+    variables = variables.split(",")
+    # Get a list of variable types, split them into array
+    types = input("Provide The Variable Types In Order(str,int): ")
+    types = types.replace(" ", "")
+    types = types.split(",")
+    # Go through and create the returned string
+    POSTDATA = """ "{ """
+    for i in range(0, len(variables)):
+        if "string" in types[i] or "str" in types[i] or "s" in types[i]:
+            POSTDATA += """ \\" """ + variables[i] + """ \\":\\""+ """ + XMLVAR + """ +"\\", """
+        elif "integer" in types[i] or "int" in types[i] or "i" in types[i]:
+            POSTDATA += """ \\" """ + variables[i] + """ \\":"+ """ + XMLVAR + """ +", """
+    # Remove the final comma, add a cap, and remove spaces
+    POSTDATA = POSTDATA.replace(" ", "")
+    POSTDATA = POSTDATA[:-1]
+    POSTDATA += """ }" """
+    POSTDATA = POSTDATA.replace(" ", "")
+    #####
+    print("----------------------------------------")
+    print("The Base URI Is: ", BASEURI)
+    print("The Request URI Is: ", REQUESTURI)
+    print("The POST Data Is: ", POSTDATA)
+    return
+####################################################################################################################################
+# This is a call to main to get the ball rolling
+if __name__ == '__main__':
+    print("PROGRAM START")
+    main()
+    print("PROGRAM END")
+# END FILE
+####################################################################################################################################
